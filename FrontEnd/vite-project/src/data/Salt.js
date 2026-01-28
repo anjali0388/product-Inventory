b1d0128eb6dcdc0b920b7409db76663b4f4232e0
Anjali yadav <anjaliyadav08905@gmail.com>
2026-01-28 15:56:06 +0530

    Removed frontend folder

--- FrontEnd/vite-project/src/data/Salt.js
+++
@@ -1,9 +0,0 @@
-const salt = [
-  { id: 1, name: "Whole Farm Premium Black Salt (100g)", image: "/namak/BlackSalt.avif", rating: 4.0, price:"₹14" },
-  { id: 2, name: "Tata Salt Iron Health(1kg)", image: "/namak/Iron.avif", rating: 4.0, price:"₹30" },
-  { id: 3, name: "Himalayan Pink Salt (500g)", image: "/namak/PinkSalt.avif", rating: 4.0, price:"₹65" },
-  { id: 4, name: "Puro Healthy Rock Salt (1kg)", image: "/namak/RockSalt.avif", rating: 4.0, price:"₹110" },
-  { id: 5, name: "Tata Himalayan Rock Pink Salt (1kg)", image: "/namak/Sendha.avif", rating: 4.0, price:"₹96" },
-  { id: 6, name: "Catch Sprinkler Table Salt (200g)", image: "/namak/TableSalt.avif", rating: 4.0, price:"₹48" },
-];
-export default salt;
